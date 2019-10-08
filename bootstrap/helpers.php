52fff89c14f7cf52386cd3ca16463f85ed88d857
eli <tsukihime@gmail.com>
2019-10-08 16:23:42 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -14,4 +14,11 @@ function tsukihime(string $something = '')
     return 'Tsukihime!';
 }
 
-
+/**
+ * 将当前请求的路由名称转换为 CSS 类名称, 作用是允许我们针对某个页面做页面样式定制
+ * 
+ */
+function route_class()
+{
+    return str_replace('.', '_', Route::currentRouteName());
+}
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+<h1>This is First Pages<h1/>
+
+@stop
