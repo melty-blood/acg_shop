f251bdb3906340157ba4881310f681beffc41dbd
eli <tsukihime@gmail.com>
2019-10-08 11:38:05 +0800

    辅助函数

---
+++ bootstrap/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * acg-shop initialize test.
+ *
+ * @param string $something
+ * @return string
+ */
+function tsukihime(string $something = '')
+{
+    if ($something !== '') {
+        return $something;
+    }
+    return 'Tsukihime!';
+}
+
+
